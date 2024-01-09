71a8ca04f7acb1cc5a448984005514ae2f22ca9b
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-01-09 09:45:16 +0100

    loading

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,9 +1,15 @@
 const url = 'https://vinepair.com/articles/50-most-popular-cocktails-world-2017/'
 
-const DrinksPage = async () => {
+const fetchDrinks = async () => {
+  await new Promise ((resolve) => setTimeout (resolve,1000));
   const response = await fetch(url);
   const data = await response.json();
-  console.log('data')
+  return DataTransferItemList
+}
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
+  console.log(data);
     return (
       <div>
         <h1 className="text-7xl">DrinksPage</h1>
