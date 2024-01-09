f8d6e3639529cc865d98279172e01f7b7f04d267
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-01-09 03:53:58 +0100

    fetch data

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,9 @@
-const DrinksPage = () => {
+const url = 'https://vinepair.com/articles/50-most-popular-cocktails-world-2017/'
+
+const DrinksPage = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log('data')
     return (
       <div>
         <h1 className="text-7xl">DrinksPage</h1>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,12 +6,12 @@ const links = [
     {href: '/tasks', label: 'tasks'},
     {href: '/query', label: 'query'},
 ];
-import React from 'react'
+
 
 const Navbar = () => {
   return (
     <nav className='bg-base-300 py-4'>
-      <div className='navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row'>\
+      <div className='navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row'>
       <Link href='/' className='btn btn-primary'>
       Next.js
       </Link>
@@ -19,7 +19,7 @@ const Navbar = () => {
         {links.map((link) =>{
         return <li key ={link.href}>
           <Link href={link.href} className='capitalize'>
-          
+          {link.label}
           </Link>
         </li>
        })}
@@ -27,7 +27,7 @@ const Navbar = () => {
       </ul>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar
